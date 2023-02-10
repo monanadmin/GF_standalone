fa8db174677290140e37bb1ccbbee91e5208c235
Denis Eiras <denis.eiras@gmail.com>
2023-02-10 16:17:02 -0300

    incluindo run_gf_times_with_hpctoolkit.sh

---
+++ scripts/run_gf_times_with_hpctoolkit.sh
@@ -0,0 +1,78 @@
+# script to execute gf.x sometimes with HPCToolkit profiler
+
+# MONAN
+#
+# Author: Denis Eiras
+#
+# E-mail: denis.eiras@inpe.br  
+#
+# Date: 2023/02/08 
+#
+# Version: TODO -> numero_da_versão_em_git_flow
+#
+
+# Full description
+# =================
+# This script executes locally gf.x 3 times (configurable) with HPCToolkit profile on. 
+#
+# Observe:
+# - Must load HPCToolkit module before. 
+#   ex: module load hpctoolkit-2021.10.15-gcc-9.4.0-i3utntd
+# - The execution must be done in the original folder (scripts)
+#
+# Usage: ./run_gf_times_with_hpctoolkit.sh
+#
+#
+# History
+# =======
+# 2023/02/08 - Initial commit - deniseiras
+#
+#
+# Licence
+# =======
+#
+# <img src="https://www.gnu.org/graphics/gplv3-127x51.png" width="63">
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the  Free  Software  Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it  will be useful, but
+# ** WITHOUT  ANY  WARRANTY **;  without  even  the   implied   warranty  of
+# **MERCHANTABILITY** or **FITNESS FOR A  PARTICULAR PURPOSE**.  See  the, GNU
+# GNU General Public License for more details.
+#
+# You should have received a copy  of the GNU General  Public  License
+# along with this program.  If not, see [GNU Public    License](https://www.gnu.org/licenses/gpl-3.0.html).
+#
+
+# parameters
+times_to_execute=3
+
+# code
+ln -fs ../gf.inp
+ln -fs ../GF_ConvPar_nml
+ln -fs ../GATE.dat
+
+hpct_measure="hpctoolkit-gf.x-measurements"
+hpct_struct="gf.x.hpcstruct"
+hpct_db="hpctoolkit-gf.x-database"
+
+rm -rf executions 
+
+for counter in $(seq 1 $times_to_execute); do 
+  echo -e "\n\n\nExecution of profiler on gf.x # $counter"
+
+  hpcrun -t ../bin/gf.x 
+  hpcstruct ../bin/gf.x 
+  hpcprof -I . -S "${hpct_struct}" "${hpct_measure}"
+
+  dir_exec="executions/exec_${counter}"
+  mkdir -p ${dir_exec}
+  mv $hpct_measure $hpct_struct ref_g.* ${hpct_db} "${dir_exec}"
+
+done
+
+
+echo -e "\n\n\nFinished!!! \nCheck generated databases in executions folder\n\ne.g:\nhpcviewer ${dir_exec}/hpctoolkit-gf.x-database\n\n"
