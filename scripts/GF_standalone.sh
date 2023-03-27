7d285db706b1aded7b8a2c5e4d9b7647426b511c
egkhamis <egkhamis@gmail.com>
2023-03-27 18:50:12 -0300

    #190 - inclusão de escolha do compilador (gnu, intel) no script GF_standalone.sh

--- scripts/GF_standalone.sh
+++ scripts/GF_standalone.sh
@@ -18,11 +18,22 @@ SRC=${DIRHOME}/src
 BIN=${DIRHOME}/bin
 
 
+# Verificando o argumento de entrada
+if [ -z "${1}" ]
+then
+  echo "COMPILER is not set: gnu or intel" 
+  exit 1
+else
+  COMPILER=`echo $1 | awk '{print $1}' `
+  echo "COMPILER=$COMPILER"
+fi
+
+
 cd ${BIN}
 rm -f ${BIN}/gf.x
 echo "Compilando"
-#make clean
-make gnu
+make clean
+make $COMPILER
 sleep 3
 
 
